3604787bd08bfc0f309c51c1fa79a114a518e255
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-11-11 22:17:43 +0000

    Import Css for styling

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React from "react";
+import css from './css/Sidebar.module.css';
 
-const Sidebar = () => <h1>It works!</h1>;
+function Sidebar() {
+    return (
+        <div className= {css.sidebar}>
+            <a href="#." target="_blank">My Photos</a>
+            <a href="#." target="_blank">My Illustrations</a> 
+            <a href="#." target="_blank">My Paintings</a>
+        </div>
+)
+}
 
 export default Sidebar;
